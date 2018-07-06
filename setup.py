e8a4476e417bd7c5012f55fac17ce2d797fce965
mikelovskij <snao20@hotmail.com>
2018-07-06 14:53:07 +0200

    added setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='cavparams',
       version='0.1',
       description='Simple estimators of FP cavity parameters',
-      #url='http://github.com/storborg/funniest',
+      url='https://github.com/mikelovskij/cavparams',
       author='Michele Valentini',
       author_email='michele.valentini@unitn.it',
       license='MIT',
